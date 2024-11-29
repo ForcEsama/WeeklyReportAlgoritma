c14acb33d2ecad082b1b6074927cfa509110a524
ForcEsama <dirgantara291@gmail.com>
2024-11-29 15:21:36 +0700

    tugas

---
+++ modul 2/jobsheet2_4.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main()
+{
+    int panjang = 10;
+    int lebar = 5;
+    int luas = (panjang * lebar);
+
+    printf("luas persegi panjang dengan panjang %d dan lebar %d adalah %d cm^2\n", panjang, lebar, luas);
+}
